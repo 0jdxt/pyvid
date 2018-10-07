15af7b55327ce952a75012a204d45110cc7edfb5
jay <4650251+xxxcoolie@users.noreply.github.com>
2018-10-08 00:46:37 +0100

    extract requirements for docs

--- get_requirements.py
+++ get_requirements.py
@@ -5,7 +5,15 @@ with open('pyproject.toml', 'r') as f:
 
 reqs = []
 for dep in data['dependencies']:
-    reqs.append(f'{dep}=={data["dependencies"][dep][1:]}')
+    reqs.append(dep + '==' + data['dependencies'][dep][1:])
 
 with open('requirements.txt', 'w') as f:
     print('\n'.join(reqs), file=f)
+
+
+dreqs = []
+for ddep in data['dev-dependencies']:
+    dreqs.append(ddep + '==' + data['dev-dependencies'][ddep][1:])
+
+with open('requirements-dev.txt', 'w') as f:
+    print('\n'.join(dreqs), file=f)
