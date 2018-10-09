9243c22fc7850ca27278530569183ae4c071ed22
jay <4650251+xxxcoolie@users.noreply.github.com>
2018-10-09 23:39:57 +0100

    updaet test :accept:

--- test/test_pyvid.py
+++ test/test_pyvid.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 import pytest
 from hurry.filesize import size
@@ -32,4 +33,4 @@ def test_cli_file_in() -> None:
 
         res = runner.invoke(main, "Carne_job.mp4 -y".split())
         assert res.exit_code == 0
-        assert "Carne_job.mp4:9878103:9117977" in set(res.output.split("\n"))
+        assert re.search("^Carne_job.mp4:9878103:9117977$", res.output, re.M)
